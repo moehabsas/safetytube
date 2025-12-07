fd8fc62780f30f9bbdb87f14a5ed208cad3db167
Mohamed El-Gedawy <moehabsas@gmail.com>
2025-12-07 22:11:51 +0200

    Change logo image format to `webp`

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function WebLogo({ size }: { size: number }) {
   return (
     <Image
-      src="/logo.svg"
+      src="/logo.webp"
       alt="شعار السلامة والصحة المهنية"
       width={size}
       height={size}
