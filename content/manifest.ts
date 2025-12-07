f9c3819aa39068c4cb6ef62bad3b520ec3436c10
Mohamed El-Gedawy <moehabsas@gmail.com>
2025-12-07 21:31:38 +0200

    Add flexible homepage

---
+++ content/manifest.ts
@@ -0,0 +1,11 @@
+export interface Manifest {
+  motto: React.ReactNode;
+  note: React.ReactNode;
+}
+
+const manifest: Manifest = {
+  motto: "اسلامة أولاً",
+  note: "للحفاظ علي سلامتكم يرجي قراءة التعليمات وإتباعها بدقة.",
+};
+
+export default manifest;
